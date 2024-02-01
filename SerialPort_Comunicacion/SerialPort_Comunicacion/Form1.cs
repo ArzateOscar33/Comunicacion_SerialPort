761c3592f44a10cdfd0521d21f12a29fc7774818
Lorenzo Diaz <124218264+LADR21@users.noreply.github.com>
2024-02-01 15:00:51 -0800

    Creacion clase Validacion
    Se creo una clase llamada Validacion en la cual se encuentran todas las validaciones del sistema
    Tambien se crearon las llamadas correspondientes, solo falta que el textbox se active a la hora de tener el COM correcto

--- SerialPort_Comunicacion/SerialPort_Comunicacion/Form1.cs
+++ SerialPort_Comunicacion/SerialPort_Comunicacion/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Security.Policy;
 
 namespace SerialPort_Comunicacion
 {
@@ -15,17 +16,13 @@ namespace SerialPort_Comunicacion
     {
         //Creamos las variables necesarias
         string dataOut;
-        string dataIN;
 
-        //Creamos un arreglo para guardar el nombre de los puertos
-        string[] ports = SerialPort.GetPortNames();
-        private List<String> portsV = new List<String>() ;
-        private List<SerialPort> SPort = new List<SerialPort>() ;
-        
+        //Llamamos a la clase
+        Validacion v = new Validacion();
         public Form1()
         {           
             InitializeComponent();
-            ValidacionCOM();
+            Validacion();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -34,12 +31,17 @@ namespace SerialPort_Comunicacion
             //Agregamos el arreglo de ports a nuestra ComboBox para que aparezcan los puertos
             //cBoxCOMPORT.Items.AddRange(ports);
 
-
+            
+            
             rbAddToOldData.Checked = true;
             rbAlwaysUpdate.Checked = false;
 
         }
+        public async void Validacion()
+        {
+            v.ValidacionCOM();
 
+        }
         private void btnOpen_Click(object sender, EventArgs e)
         {
             try
@@ -85,7 +87,7 @@ namespace SerialPort_Comunicacion
 
         private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            dataIN = serialPort1.ReadExisting();
+            v.dataIN = serialPort1.ReadExisting();
             //Este metodo nos mostrara los datos en el textbox , para ello es necesario crear un metodo privado 
             //sin este metodo privado no se puede mostrar los datos 
             this.Invoke(new EventHandler(ShowData));
@@ -96,11 +98,11 @@ namespace SerialPort_Comunicacion
 
             if (rbAlwaysUpdate.Checked)
             {
-                txbReceive.Text = dataIN;
+                txbReceive.Text = v.dataIN;
             }
             else if (rbAddToOldData.Checked)
             {
-                txbReceive.Text += dataIN;
+                txbReceive.Text += v.dataIN;
             }
         }
 
@@ -129,63 +131,6 @@ namespace SerialPort_Comunicacion
                 txbReceive.Text = "";
             }
         }
-        public void CPSerial()
-        {
-           for(int i=0;i<ports.Length;i++)
-           {
-                SerialPort _Sp = new SerialPort();
-           }
-        }
-        public async void ValidacionT(SerialPort SP)
-        {
-            //Abrimos la conexion
-            SP.Open();
-            //leemos el dato de entrada 
-            dataIN += SP.ReadExisting();
-            await Task.Delay(10000);
-            if (dataIN.StartsWith("{REQ100"))
-            {
-               // portsV.Add(COM);
-               // MessageBox.Show("El puerto" + COM + " esta habilitado ");
-            }
-            else
-            {
-                MessageBox.Show("No hay ningun COM Conectado");
-            }
-            serialPort1.Close();
-        }
-        public void ValidacionCOM()
-        {
-            /* try
-             { */
-            //cBoxCOMPORT.Enabled = false;
-            
-            foreach (string COM in ports)
-            {
-                var port = new SerialPort()
-                {
-                    //string COM = "COM4";
-                    //creamos la conexion 
-                    PortName = COM,
-                    BaudRate = 115200,
-                    DataBits = 8,
-                    StopBits = StopBits.One,
-                    Parity = Parity.None
-                };
-                SPort.Add(port);
-                ValidacionT(port);
-            }//foreach
-            
-                MessageBox.Show("Puertos Habilitados");
-                //cBoxCOMPORT.Text = portsV.ToString();
-                cBoxCOMPORT.DataSource = portsV;
-                cBoxCOMPORT.Enabled = true;
-            //}//try
-            /*catch (Exception ex)
-            {
-                //En caso de que nos de error nos abrira un MessageBox con el error 
-                MessageBox.Show(ex.Message, "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }//catch*/
-        }
+         
     }
 }
